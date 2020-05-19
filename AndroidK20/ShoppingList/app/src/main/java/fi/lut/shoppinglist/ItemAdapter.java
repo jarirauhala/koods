30ef1f0782dfd5487f225f3cd4745b78034fc2e2
Jari Rauhala <jari.rauhala@tuni.fi>
2020-05-19 15:48:40 +0300

    ItemAdapter and layout initialized with test functionaliy

---
+++ AndroidK20/ShoppingList/app/src/main/java/fi/lut/shoppinglist/ItemAdapter.java
@@ -0,0 +1,50 @@
+package fi.lut.shoppinglist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ItemAdapter extends BaseAdapter {
+
+    LayoutInflater mInflater;
+    ArrayList<String> mTestList;
+
+
+    public ItemAdapter(Context c, ArrayList<String> testList) {
+        mTestList = testList;
+        mInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return mTestList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mTestList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v  = mInflater.inflate(R.layout.layout_description, null);
+        TextView testTextView = (TextView) v.findViewById(R.id.testTextView);
+
+        String name = mTestList.get(position);
+
+        testTextView.setText(name);
+
+        return v;
+    }
+}
--- AndroidK20/ShoppingList/app/src/main/java/fi/lut/shoppinglist/MainActivity.java
+++ AndroidK20/ShoppingList/app/src/main/java/fi/lut/shoppinglist/MainActivity.java
@@ -3,12 +3,32 @@ package fi.lut.shoppinglist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // to hold some data for displaying in ListView
+    ArrayList<String> testList = new ArrayList<String>();
+
+    ListView itemsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        itemsListView = (ListView) findViewById(R.id.itemsListView);
+
+        testList.add("Eka");
+        testList.add("Toka");
+        testList.add("Vika");
+
+        ItemAdapter itemAdapter = new ItemAdapter(this, testList);
+
+        itemsListView.setAdapter(itemAdapter);
+
+
     }
 }
